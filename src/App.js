184127413e13854a6e01a357053e209385e4f821
whrgo <dev.whrgo@gmail.com>
2022-06-01 12:42:43 +0900

    train for f[useEffect]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+    const [counter, setValue] = useState(0); // initional value : 0
+    const onClick = () => setValue(prev => prev + 1);
+
+    /* STATE FUNCTION OF F('Search ') */
+    const [keyword, setKeyword] = useState(""); // initional value : ' '
+    const onChange = event => setKeyword(event.target.value);
+
+    useEffect(() => {
+        console.log("I run only once.");
+    }, []);
+    useEffect(() => {
+        console.log("I run when 'keyword' changes");
+    }, [keyword]);
+    useEffect(() => {
+        console.log("I run when 'counter' changes");
+    }, [counter]);
     return (
         <div>
-            <h1 className={styles.title}>Welcome back!</h1>
-            <Button text="Continue" />
+            <input
+                value={keyword}
+                onChange={onChange}
+                type="text"
+                placeholder="Search here..."
+            />
+            <h1>{counter}</h1>
+            <button onClick={onClick}>click me</button>
         </div>
     );
 }
